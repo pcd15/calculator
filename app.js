7eea0257dce9ec842375acb8569df0442208755c
Paul Dilly <paul.dilly@duke.edu>
2023-08-04 16:14:13 -0400

    dunne

--- app.js
+++ app.js
@@ -15,12 +15,26 @@ const buttons = document.querySelectorAll('.button');
 toggle.addEventListener('click', switchMode);
 
 function updateDisplay() {
-    if (count == 0) {
+    if (afterEquals || (count == 0 && first.length == 10) || (count == 1 && second.length == 10)) {
+        return;
+    } else if (count == 0) {
         first += this.id;
         result.textContent = first;
     } else {
         second += this.id;
         result.textContent = second;
+    }
+}
+
+function getSign() {
+    if (sign === "plus") {
+        return " + ";
+    } else if (sign === "minus") {
+        return " - ";
+    } else if (sign === "multiply") {
+        return " x ";
+    } else {
+        return " / ";
     }
 }
 
@@ -32,55 +46,83 @@ function handleSign() {
         sign = this.id;
     } else {
         sign = this.id;
-        firstNum = Number(first);
         result.textContent = "";
-        currExpression.push(firstNum);
-        currExpression.push(sign);
+        currExpression += first;
+        currExpression += getSign();
         count++;
+        if (afterEquals) {
+            expression.textContent = "";
+            afterEquals = false;
+            afterSign = false;
+        }
+        if (afterSign) {
+            afterSign = false;
+        }
     }
 }
-
-// fix: 
-// * operations are always undefined
-// * change currExpression from array to string
-// * change how currExpression is formatted (comes partly w/ changing from array to string)
-
-// with delete, don't worry about removing signs (only ends of numbers)
-// C just resets everything (hopefully this is easy to implement)
-// maybe add check that prevents user from entering more than 10 digits per number (e.g., str.length === 10)?
 
 function equals() {
     if (first === "" || second === "") {
         return;
     } else {
-        secondNum = Number(second);
-        currExpression.push(secondNum);
-        expression.textContent = currExpression.toString();
+        currExpression += second;
+        expression.textContent = currExpression;
         if (sign === "plus") {
-            firstNum += secondNum;
+            num = Number(first) + Number(second);
         } else if (sign === "minus") {
-            firstNum -= secondNum;
+            num = Number(first) - Number(second);
         } else if (sign === "multiply") {
-            firstNum *= secondNum;
+            num = Number(first) * Number(second);
         } else {
-            firstNum /= secondNum;
+            num = Number(first) / Number(second);
         }
-        result.textContent = toString(firstNum);
-        currExpression = [];
-        currExpression.push(toString(firstNum));
-        secondNum = -1;
+        num = Math.round(num * 100) / 100;
+        first = num.toString();
+        result.textContent = first;
+        currExpression = "";
         sign = "";
+        second = "";
         count = 0;
+        afterEquals = true;
+        afterSign = true;
     }
 }
 
+function deletePrev() {
+    if (afterSign || first === "" || (second === "" && count == 1)) {
+        return;
+    } else if (count == 0) {
+        first = first.substring(0, first.length - 1);
+        result.textContent = first;
+        currExpression = currExpression.substring(0, currExpression.length - 1);
+    } else {
+        second = second.substring(0, second.length - 1);
+        result.textContent = second;
+        currExpression = currExpression.substring(0, currExpression.length - 1);
+    }
+}
+
+function reset() {
+    result.textContent = "";
+    expression.textContent = "";
+    afterEquals = false;
+    afterSign = false;
+    count = 0;
+    num = 0;
+    first = "";
+    sign = "";
+    second = "";
+    currExpression = "";
+}
+
+let afterEquals = false;
+let afterSign = false;
 let count = 0;
-let sign = "";
-let currExpression = [];
+let num = 0;
 let first = "";
+let sign = "";
 let second = "";
-let firstNum = -1;
-let secondNum = -1;
+let currExpression = "";
 
 const expression = document.querySelector('#expression');
 const result = document.querySelector('#result');
@@ -93,3 +135,9 @@ signBtns.forEach(button => button.addEventListener('click', handleSign));
 
 const equalsBtn = document.querySelector('#equals');
 equalsBtn.addEventListener('click', equals);
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', reset);
+
+const deleteBtn = document.querySelector('#delete');
+deleteBtn.addEventListener('click', deletePrev);
